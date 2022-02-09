7ca8098510fd8f08350d94feedc121448374505a
farazkabir <45370774+farazkabir@users.noreply.github.com>
2022-02-09 17:33:12 +0600

    Bug fix

---
+++ src/activities.js
@@ -0,0 +1,16 @@
+let info = {
+    "23 January, 2022":"Our team got qualified for the SAR round in the URC-2022 competition",
+    "6 January, 2022": "Our team became shortlisted in the FS AI 2022(Driverless Vehicle)",
+    "3 January, 2022": "Graduated from Ahsanullah University of Science and Technology",
+    "December 2021": "Our rover got selected as the Top-5 industrial projects in IC4IR 2021",
+    "July 2021": "Left job at BiTechX",
+    "September 2020": "Collaborated with the MindcraftLabs to conduct a workshop",
+    "July 2020":"One <a class='text-t-red' target='_blank' href='https://www.youtube.com/playlist?list=PLlW3il1pE_XQUyGRwwlrAVd9MXt8KXeHz'>course</a> got published",
+    "January 2020": "Got promoted as a senior front-end web developer",
+    "November 2019": "Joined team AUST Mars rover",
+    "October 2019": "Co-founded Circuit Maesters",
+    "August 2019": "Became runners-up in a segment in <em>Codeware'19</em> ",
+    "January 2019": "Started working at BiTechX",
+    "July 2018": "Our team became runners-up in the University website developer hunt competition",
+    "Februay 2018": "One project got honorable mention in the  Intra AUST competition <em>Technomaina'18</em>"
+}
